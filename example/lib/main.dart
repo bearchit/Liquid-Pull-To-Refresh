721670d475e47f8c8ee557855df6d26bd0c70f3f
bearchit <bearchit@gmail.com>
2019-06-20 09:53:11 +0900

    Add missing scaffold key in an example

--- example/lib/main.dart
+++ example/lib/main.dart
@@ -69,6 +69,7 @@ class _MyHomePageState extends State<MyHomePage> {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
+      key: _scaffoldKey,
       appBar: AppBar(
         title: Stack(
           children: <Widget>[
